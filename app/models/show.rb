8c8e82be70b494026b458c3157686c805472594f
Jawann <jawann@gmail.com>
2018-04-04 01:11:09 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -27,9 +27,3 @@ class Show < ActiveRecord::Base
     Show.order(:name)
   end
 end
-
-#lowest_rating: returns the lowest value in the ratings column.
-#least_popular_show: returns the show with the lowest rating.
-#ratings_sum: returns the sum of all of the ratings.
-#popular_shows: returns an array of all of the shows that have a rating greater than 5. hint: use the #where Active Record method.
-#shows_by_alphabetical_order: returns an array of all of the shows sorted by alphabetical order according to their names. hint: use the #order Active Record method.
